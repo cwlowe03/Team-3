b476512f74a37aac80eabb80bfdfac372003dbe8
rrombee <104233660+rrombee@users.noreply.github.com>
2022-04-28 20:29:50 -0400

    Delete Alex_ProjectIDeas.java

--- Alex_ProjectIDeas.java
+++
@@ -1,5 +0,0 @@
-public class Alex_ProjectIDeas {
-    public static void main(String[] args) {
-        System.out.println("We could do a calculator similar to the dorm plan calculator assignment");
-    }
-}
