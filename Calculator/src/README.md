cf0be164d4195468ab5cf7a3ec66d3d944cac2a4
rroma <rromero@carrollcc.edu>
2022-04-28 20:25:29 -0400

    Created JavaFX for the calculator project.

---
+++ Calculator/src/README.md
@@ -0,0 +1,4 @@
+## Team-3
+Hello Team. This is Ruthanne
+Hello Team, This is Alex
+Hello Team, This is Cedric 
---
+++ Calculator/src/main/java/com/example/calculator/Alex_ProjectIDeas.java
@@ -0,0 +1,6 @@
+public class Alex_ProjectIDeas {
+    public static void main(String[] args) {
+        System.out.println("We could do a calculator similar to the dorm plan calculator assignment");
+        System.out.println("That idea sounds okay to me.");
+    }
+}
